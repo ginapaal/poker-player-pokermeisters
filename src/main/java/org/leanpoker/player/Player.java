4fd56bf7a677827893430f160dde97c9bbb4206f
Unknown <paalginaa@gmail.com>
2017-11-20 15:01:01 +0100

    getting card data

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -15,16 +15,23 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonObject jsonObject = request.getAsJsonObject();
-//        int currentBuyIn = jsonObject.get("current_buy_in").getAsInt();
+        int currentBuyIn = jsonObject.get("current_buy_in").getAsInt();
         JsonArray array = jsonObject.get("players").getAsJsonArray();
         System.out.println(array);
+        int bet = 0;
         for (int i = 0; i < array.size(); i++) {
             JsonObject arrayData = array.get(i).getAsJsonObject();
-            int bet = arrayData.get("bet").getAsInt();
+            bet = arrayData.get("bet").getAsInt();
             System.out.println(bet);
+            JsonArray holeCards = arrayData.get("hole_cards").getAsJsonArray();
+            for (int j = 0; j < holeCards.size(); j++) {
+                JsonObject card = holeCards.get(j).getAsJsonObject();
+                String cardRank = card.get("rank").getAsString();
+                String cardSuit = card.get("suit").getAsString();
+            }
         }
         int in_action = jsonObject.get("in_action").getAsInt();
-        System.out.println(in_action);
+        System.out.println(bet);
         return 0;
     }
 
