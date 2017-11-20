d15f2610b8a1bbff7c7a2d2a2493ed6d0df782c5
davidtanacs <tanacsdavid90@gmail.com>
2017-11-20 11:58:43 +0100

    #2

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,8 +1,10 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.List;
 import java.util.Map;
 
 public class Player {
@@ -11,9 +13,33 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonObject jsonObject = request.getAsJsonObject();
+<<<<<<< Updated upstream
         int currentBuyIn = jsonObject.get("current_buy_in").getAsInt();
         int minimumRaise = jsonObject.get("minimum_raise").getAsInt();
         return currentBuyIn + minimumRaise;
+=======
+        JsonElement inAction = jsonObject.get("in_action");
+        String stringInAction = inAction.getAsString();
+        JsonElement currBuyIn = jsonObject.get("current_buy_in");
+
+        JsonObject players = jsonObject.get("players").getAsJsonObject();
+        int intCurrBuyIn = currBuyIn.getAsInt();
+
+        JsonObject ourPlayers = players.get("name").getAsJsonObject();
+        JsonArray ourPlayerList = ourPlayers.getAsJsonArray();
+        JsonElement ourPlayerString;
+
+        for (JsonElement player: ourPlayerList
+             ) {
+            if (player.getAsString().equals("PokerMeisters")){
+                   ourPlayerString = player;
+            }
+        }
+        JsonElement ourPlayerBet = ourPlayers.get("bet");
+        int intBet = ourPlayerBet.getAsInt();
+
+        return intCurrBuyIn - intBet;
+>>>>>>> Stashed changes
     }
 
     public static void showdown(JsonElement game) {
