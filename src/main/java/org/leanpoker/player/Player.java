9906565b3b53fe28173f9f26a0ed85b521fbcc4d
Unknown <paalginaa@gmail.com>
2017-11-20 12:16:34 +0100

    first try

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,33 +13,9 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonObject jsonObject = request.getAsJsonObject();
-<<<<<<< Updated upstream
         int currentBuyIn = jsonObject.get("current_buy_in").getAsInt();
         int minimumRaise = jsonObject.get("minimum_raise").getAsInt();
         return currentBuyIn + minimumRaise;
-=======
-        JsonElement inAction = jsonObject.get("in_action");
-        String stringInAction = inAction.getAsString();
-        JsonElement currBuyIn = jsonObject.get("current_buy_in");
-
-        JsonObject players = jsonObject.get("players").getAsJsonObject();
-        int intCurrBuyIn = currBuyIn.getAsInt();
-
-        JsonObject ourPlayers = players.get("name").getAsJsonObject();
-        JsonArray ourPlayerList = ourPlayers.getAsJsonArray();
-        JsonElement ourPlayerString;
-
-        for (JsonElement player: ourPlayerList
-                ) {
-            if (player.getAsString().equals("PokerMeisters")){
-                ourPlayerString = player;
-            }
-        }
-        JsonElement ourPlayerBet = ourPlayers.get("bet");
-        int intBet = ourPlayerBet.getAsInt();
-
-        return intCurrBuyIn - intBet;
->>>>>>> Stashed changes
 
     }
 
