f89d32405709868d6090593026744e0994490192
davidtanacs <tanacsdavid90@gmail.com>
2017-11-20 10:37:20 +0100

    Try to raise.#1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,7 +10,13 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        JsonObject jsonObject = request.getAsJsonObject();
+        JsonElement currBuyIn = jsonObject.get("current_buy_in");
+        JsonObject players = jsonObject.get("players").getAsJsonObject();
+        JsonElement bet = players.get("bet");
+        int intCurrBuyIn = currBuyIn.getAsInt();
+        int intBet = bet.getAsInt();
+        return intCurrBuyIn - intBet + 5;
     }
 
     public static void showdown(JsonElement game) {
